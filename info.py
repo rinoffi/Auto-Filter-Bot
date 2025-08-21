19cffcc5eeff7247c26b7d45141ba5ad1380e0a1
sivaneshrin-ux <sivaneshrin@gmail.com>
2025-08-21 18:51:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -47,8 +47,8 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster02")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'clustor02')
 
 # If MULTIPLE_DB Is True Then Fill DATABASE_URI2 Value Else You Will Get Error.
-MULTIPLE_DB = is_enabled(os.environ.get('MULTIPLE_DB', "False"), False) # Type True For Turn On MULTIPLE DB FUNTION 
-DATABASE_URI2 = environ.get('DATABASE_URI2', "")
+MULTIPLE_DB = is_enabled(os.environ.get('MULTIPLE_DB', "True"), True) # Type True For Turn On MULTIPLE DB FUNTION 
+DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://vihara4360:waldernun@cluster0.xueft5v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/movie_series_tamil_req')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+Jo13VAOePDQ4NzJl')
